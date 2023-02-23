1c4364c60bb38ae5c65362bf3f3368e17efe9c91
AlexandreCalixto <calixto295@hotmail.com>
2023-02-22 22:43:50 -0300

    ex+

--- python_guanabara/63.maior_menor.py
+++ python_guanabara/63.maior_menor.py
@@ -1,6 +1,19 @@
-num = int(input('Digite um numero: '))
-n = ''
-while n != 'N':
-    n = str(input('Quer continuar? [S/N] ')).upper()[0].strip()
+resp = 'S'
+cont = 0
+media = soma = quant = 0
+while resp in 'Ss':
     num = int(input('Digite um numero: '))
-
+    
+    cont += 1
+    soma += num
+    if cont == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        elif num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N] ')).upper()[0].strip()
+media = soma / cont
+print(f'Voce digitou {cont} numero(s) e a media {media}')
+print(f'O maior numero foi {maior} e o menor {menor}')
---
+++ python_guanabara/65.tabuada_v3.py
@@ -0,0 +1,7 @@
+while True:
+    numero = int(input('Digite um numero para saber sua tabuada: '))
+    if numero < 0:
+        break
+    for c in range(0, 11):
+        print(f'{numero} x {c} = {numero * c}')
+print('PROGRAMA ENCERRADO. Volte Sempre!!')
