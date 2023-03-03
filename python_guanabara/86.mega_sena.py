14f7f1b52e8c7c0243ec6ac3c3643b159e084414
AlexandreCalixto <calixto295@hotmail.com>
2023-03-03 20:41:26 -0300

    new exx

--- python_guanabara/86.mega_sena.py
+++ python_guanabara/86.mega_sena.py
@@ -1,12 +1,43 @@
+#importando tempo e random
 from time import sleep
 from random import randint
-jogo = list()
-quant = int(input('Quer gerar quantos jogos? '))
-for j in range(1, quant+1):
-    while len(jogo) < 6:
-        n = randint(1, 60)
-        if n not in jogo:
-            jogo.append(n)
-    print(f'Jogo {j}: {sorted(jogo)}')
-    sleep(.5)
-    jogo.clear()
+
+#apenas um charme
+print('=-' *30)
+print(f"{'Joga na MEGA SENA' :^59}")
+print('=-' *30)
+
+#perguntando quantos jogos para sortear
+resp = int(input('Quantos Jogos você quer que eu sorteie? '))
+#charme
+print(f'=-=-=-   SORTEANDO {resp} JOGOS   -=-=-=')
+#após responder quantos jogos, o programa dará 0.5 segundos antes de iniciar o for abaixo
+sleep(0.5)
+#para c até a resposta dada pelo usuario(+1) e 0.5 segundos apos cada random
+for c in range(1, resp+1):
+    lista = []
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont +=1
+        if cont >= 6:
+            break
+    sleep(0.5)
+    lista.sort()
+    print(f'{c}° jogo: {lista}')
+
+
+
+print('-=-=-=-=  <  BOA SORTE  >  -=-=-=-=')
+
+
+# from random import randint
+# lista = []
+# cont = 0
+# while True:
+#     num = randint(1, 60)
+#     if num not in lista:
+#         lista.append(num)
+#         cont +=1
